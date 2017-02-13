9b05f4508afb0a1210302f59ab237d2a88333980
tuxuekun <tuxuekun@chinac.com>
2017-02-13 10:40:51 +0800

    首次提交

---
+++ src/Immortal/Contract/Container/BindingResolutionException.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * 容器绑定异常处理
+ */
+namespace Immortal\Contract\Container;
+
+use Exception;
+
+class BindingResolutionException extends Exception
+{
+    //
+}
---
+++ src/Immortal/Contract/Container/ContextualBindingBuilder.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * 容器关联构建接口
+ */
+namespace Immortal\Contract\Container;
+
+interface ContextualBindingBuilder
+{
+    /**
+     * Define the abstract target that depends on the context.
+     *
+     * @param  string  $abstract
+     * @return $this
+     */
+    public function needs($abstract);
+
+    /**
+     * Define the implementation for the contextual binding.
+     *
+     * @param  \Closure|string  $implementation
+     * @return void
+     */
+    public function give($implementation);
+}
